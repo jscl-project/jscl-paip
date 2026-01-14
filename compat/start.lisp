dad3bd1edc50ca4118e74935ffd4e789de82f7e1
David Vázquez Púa <davazp@gmail.com>
2026-01-14 02:20:37 +0100

    Autoload auxfns

--- compat/start.lisp
+++ compat/start.lisp
@@ -3,3 +3,8 @@
 (format t "~%Welcome to JSCL-PAIP!~%")
 (format t "Paradigms of Artificial Intelligence Programming in the browser.~%")
 (format t "~%")
+
+(format t "Loading auxfns.lisp...~%")
+(load "auxfns")
+(format t "Done. Use (requires 'filename) to load other PAIP files.~%")
+(format t "~%")
